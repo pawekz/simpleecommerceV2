f3c3d70828683d36d6f5714967afe0eacfaabc56
dtpco <donnalyn.topacio@cit.edu>
2024-03-28 19:17:07 +0800

    Removed the temp folder

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
+#dee imported messages from django.contrib
+from django.contrib import messages
 
 from .forms import CustomerRegistrationForm, SellerRegistrationForm, CustomerProfileForm, SellerProfileForm
 
@@ -11,10 +13,14 @@ def customer_register(request):
         form = CustomerRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:home')
+            messages.success(request, 'Your account has been created successfully. You can now log in.')
+            return redirect('accounts:login')
+            # dee changed the redirect to login
+        else:
+            print(form.errors)
     else:
         form = CustomerRegistrationForm()
-    return render(request, 'accounts/temp/customer_registration.html', {'form': form})
+    return render(request, 'accounts/customer_registration.html', {'form': form})
 
 
 def seller_register(request):
@@ -22,14 +28,16 @@ def seller_register(request):
         form = SellerRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:home')
+            return redirect('accounts:login')
+
+            #dee changed the redirect to login
     else:
         form = SellerRegistrationForm()
-    return render(request, 'accounts/temp/seller_registration.html', {'form': form})
+    return render(request, 'accounts/seller_registration.html', {'form': form})
 
 
 def terms_and_conditions(request):
-    return render(request, 'accounts/temp/terms_and_conditions.html')
+    return render(request, 'accounts/terms_and_conditions.html')
 
 
 def home(request):
@@ -47,10 +55,11 @@ def login_view(request):
                 login(request, user)
                 return redirect('accounts:home_dashboard')  # Redirect to a success page.
             else:
+
                 form.add_error(None, 'Invalid username or password')
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/temp/login.html', {'form': form})
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout_view(request):
@@ -86,7 +95,7 @@ def profile_view(request):
 
 
 def home_dashboard(request):
-    return render(request, 'accounts/temp/home_dashboard.html')
+    return render(request, 'accounts/home_dashboard.html')
 
 
 def customer_profile(request):
@@ -101,7 +110,7 @@ def customer_profile(request):
     else:
         form = CustomerProfileForm(instance=request.user)
         password_form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/temp/customer_profile.html', {'form': form, 'password_form': password_form})
+    return render(request, 'accounts/customer_profile.html', {'form': form, 'password_form': password_form})
 
 
 def seller_profile(request):
@@ -116,4 +125,5 @@ def seller_profile(request):
     else:
         form = SellerProfileForm(instance=request.user)
         password_form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/temp/seller_profile.html', {'form': form, 'password_form': password_form})
+    return render(request, 'accounts/seller_profile.html', {'form': form, 'password_form': password_form})
+
