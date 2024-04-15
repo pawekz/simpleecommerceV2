c5bd4960da8d3e9177104f54c4298ba588fce355
Paulo Carabuena <paulo.carabuena@cit.edu>
2024-04-15 17:35:10 +0800

    modified `models.py` in delivery and register models in each corresponding app

--- simpleecommerce/settings.py
+++ simpleecommerce/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-t)boarxp^zreu)9=!x*ddm5h17j23r@8b$ofd==_jzmxqphh!v
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,7 +39,8 @@ INSTALLED_APPS = [
     "transaction.apps.PaymentConfig",
     "cart.apps.CartConfig",
     "products.apps.ProductsConfig",
-    "delivery.apps.DeliveryConfig"
+    "delivery.apps.DeliveryConfig",
+    'dbbackup',  # django-dbbackup
 ]
 
 MIDDLEWARE = [
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'simpleecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -116,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -131,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #  this might fix the issue when creating superuser in the terminal
 AUTH_USER_MODEL = 'accounts.CustomUser'
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DBBACKUP_STORAGE_OPTIONS = {'location': str(BASE_DIR / 'backup/')}
+#testing
--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import OrderHistory, Payment, Transaction
 # Register your models here.
+
+admin.site.register(OrderHistory)
+admin.site.register(Payment)
+admin.site.register(Transaction)
+
