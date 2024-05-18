dfe4cd6491255c883fdeab5f12418cd1f846be83
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2024-05-19 07:30:18 +0800

    added urls in transaction

--- transaction/urls.py
+++ transaction/urls.py
@@ -6,9 +6,12 @@ app_name = 'transaction'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('payment_option/', views.payment_option, name='payment_option'),
     path('gcash_payment/', views.gcash_payment, name='gcash_payment'),
     path('maya_payment/', views.maya_payment, name='maya_payment'),
     path('cod_payment/', views.cod_payment, name='cod_payment'),
     path('credit_payment/', views.credit_payment, name='credit_payment'),
+    path('review_cart/', views.review_cart, name='review_cart'),
+    path('confirm_payment/', views.confirm_payment, name='confirm_payment'),
     path('success/', views.success, name='success'),
 ]
